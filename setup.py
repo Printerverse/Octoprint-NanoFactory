b1608ed760228e8ec814b4e048b30b3e0a0f334d
thakkaryash21 <thakkaryash21@gmail.com>
2022-11-28 17:41:57 +0530

    removed open cv entirely

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ plugin_url = "https://github.com/Printerverse/Octoprint-NanoFactory/"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["selenium", "opencv-python"]
+plugin_requires = ["selenium"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
