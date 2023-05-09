6d0eee1b5745550a6ff123211e8b7851ebd7c010
thakkaryash21 <thakkaryash21@gmail.com>
2023-05-09 15:21:04 +0530

    quotes around url and more logs

--- octoprint_NanoFactory/Utilities.py
+++ octoprint_NanoFactory/Utilities.py
@@ -108,7 +108,7 @@ def get_browser_path(operating_system: Literal["Windows", "Darwin", "Linux"]):
 
 def start_browser(operating_system: Literal["Windows", "Darwin", "Linux"], api_key: str, peer_ID: str, master_peer_id: str, base_url: str):
 
-    url = 'file:///{}?baseURL={}&apiKey={}&peerID={}&masterPeerID={}'.format(
+    url = '"file:///{}?baseURL={}&apiKey={}&peerID={}&masterPeerID={}"'.format(
         index_html_file_path, urllib.parse.quote(base_url, safe=""), api_key, peer_ID, master_peer_id)
 
     from . import __plugin_implementation__ as plugin
@@ -154,6 +154,8 @@ def start_browser(operating_system: Literal["Windows", "Darwin", "Linux"], api_k
             return process.as_dict()["pid"]
 
         except Exception as e:
+            plugin._logger.error(
+                "Error while opening browser. Trying using an alternative method", exc_info=True)
 
             try:
                 subprocess.run(
