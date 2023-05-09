65726909d87ee61658a5218cd66a5f73514ab699
thakkaryash21 <thakkaryash21@gmail.com>
2023-05-09 16:23:04 +0530

    using firefox for linux

--- octoprint_NanoFactory/Utilities.py
+++ octoprint_NanoFactory/Utilities.py
@@ -141,43 +141,47 @@ def start_browser(operating_system: Literal["Windows", "Darwin", "Linux"], api_k
 
     if operating_system == "Linux":
         try:
-            browser_path = get_browser_path(operating_system)
-
-            if not browser_path:
-                from . import __plugin_implementation__ as plugin
-                plugin._logger.error(
-                    linux_chrome_path_1 + " or " + linux_chrome_path_2 + " not found.")
-                return
-
-            plugin._logger.info("Opening browser with command: " +
-                                str([browser_path, url] + (get_browser_flags()).split(" ")))
-
-            process = psutil.Popen([browser_path, url] + (get_browser_flags()).split(" "), stdin=subprocess.PIPE,
-                                   stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-            output, error = process.communicate()
-            plugin._logger.info(f"output: {output.decode('utf-8')}")
-            plugin._logger.info(f"error: {error.decode('utf-8')}")
-            plugin._logger.info(
-                "NanoFactory browser started with PID: " + str(process.as_dict()["pid"]))
+            process = psutil.Popen(["firefox", url], stdin=subprocess.PIPE,
+                                   stdout=subprocess.DEVNULL,  stderr=subprocess.PIPE)
             return process.as_dict()["pid"]
+            # browser_path = get_browser_path(operating_system)
+
+            # if not browser_path:
+            #     from . import __plugin_implementation__ as plugin
+            #     plugin._logger.error(
+            #         linux_chrome_path_1 + " or " + linux_chrome_path_2 + " not found.")
+            #     return
+
+            # plugin._logger.info("Opening browser with command: " +
+            #                     str([browser_path, url] + (get_browser_flags()).split(" ")))
+
+            # process = psutil.Popen([browser_path, url] + (get_browser_flags()).split(" "), stdin=subprocess.PIPE,
+            #                        stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+            # output, error = process.communicate()
+            # plugin._logger.info(f"output: {output.decode('utf-8')}")
+            # plugin._logger.info(f"error: {error.decode('utf-8')}")
+            # plugin._logger.info(
+            #     "NanoFactory browser started with PID: " + str(process.as_dict()["pid"]))
+            # return process.as_dict()["pid"]
 
         except Exception as e:
             plugin._logger.warning(
-                "Error while opening browser using psutil.", exc_info=True)
+                "Error while opening browser using psutil.")
+            plugin._logger.warning(e, exc_info=True)
 
-            try:
-                plugin._logger.info("{} {} {}".format(
-                    browser_path, url, get_browser_flags()))
-                subprocess.run(
-                    "{} {} {}".format(browser_path, url, get_browser_flags()), shell=True
-                )
-                plugin._logger.info(
-                    "NanoFactory browser started using subprocess.")
+            # try:
+            #     plugin._logger.info("{} {} {}".format(
+            #         browser_path, url, get_browser_flags()))
+            #     subprocess.run(
+            #         "{} {} {}".format(browser_path, url, get_browser_flags()), shell=True
+            #     )
+            #     plugin._logger.info(
+            #         "NanoFactory browser started using subprocess.")
 
-            except Exception as e:
-                from . import __plugin_implementation__ as plugin
-                plugin._logger.error("Error while opening chromium.")
-                plugin._logger.error(e, exc_info=True)
+            # except Exception as e:
+            #     from . import __plugin_implementation__ as plugin
+            #     plugin._logger.error("Error while opening chromium.")
+            #     plugin._logger.error(e, exc_info=True)
 
 
 def close_browser(pid: int, operating_system: Literal["Windows", "Darwin", "Linux"]):
